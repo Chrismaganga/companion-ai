117385683eaf39c8b1b5709dffb6f2630f056a60
Chrismaganga <chrismaganga3@gmail.com>
2023-08-17 21:37:30 +0200

    update

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,16 @@
+import { UserButton } from "@clerk/nextjs"
+
+
+type Props = {}
+
+const RootPage = (props: Props) => {
+  return (
+    <div>
+    hello
+
+
+    </div>
+  )
+}
+
+export default RootPage
